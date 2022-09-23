8a6c3b2743da11b17aafcfaf5afb2ecb3affbb2b
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-09-23 18:38:34 +0700

    Add project files.

---
+++ JWTRefreshTokenDemo/Auth/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTRefreshTokenDemo.Auth
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ JWTRefreshTokenDemo/Auth/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTRefreshTokenDemo.Auth
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
